17982356339e3de97c6b9dc8900145ce59edc262
ExquisitePixelz <74294569+ExquisitePixelz@users.noreply.github.com>
2024-05-09 16:27:24 +0200

    Tried some things, did not work

---
+++ colocation-sample-fusion/Assets/NetworkInputData.cs
@@ -0,0 +1,8 @@
+using Fusion;
+using UnityEngine;
+
+public struct NetworkInputData : INetworkInput
+{
+    public const byte MOUSEBUTTON0 = 1;
+    public NetworkButtons buttons;
+}
---
+++ colocation-sample-fusion/Assets/ShareAnchors.cs
@@ -0,0 +1,42 @@
+using com.meta.xr.colocation;
+using com.meta.xr.colocation.fusion;
+using Fusion;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Meta.XR.BuildingBlocks
+{
+    public class ShareAnchors : NetworkBehaviour
+    {
+        private SharedAnchorManager _sharedAnchorManager;
+        public FusionNetworkData _fusionNetworkData;
+        public GameObject[] spawnables;
+        public Transform handAnchor;
+        private List<OVRSpatialAnchor> _OVRSpatialAnchors = new();
+        private List<OVRSpaceUser> _OVRSpaceUsers = new();
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _sharedAnchorManager = FindObjectOfType<SharedAnchorManager>();
+            _fusionNetworkData = FindObjectOfType<FusionNetworkData>();
+        }
+
+        // Update is called once per frame
+        public void SpawnObjectAndShareSpatialAnchors(GameObject SpawnedObject)
+        {
+            _sharedAnchorManager.InstantiateSpatialAnchor(SpawnedObject, handAnchor.position, handAnchor.rotation);
+            _OVRSpatialAnchors.Add(SpawnedObject.GetComponent<OVRSpatialAnchor>());
+            _sharedAnchorManager.ShareSpatialAnchors(_OVRSpatialAnchors, _sharedspatialAnchorCore._userShareList);
+
+
+            //_spatialAnchorCore.ShareSpatialAnchors();
+
+
+        }
+    }
+}
+
